bd822aa8c0664e4caab3fb79f642f42c57d29643
Victory <victrudoh@gmail.com>
2023-02-23 17:57:46 +0100

    changed uri and added 'npm start' to package.json for render

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const mongoose = require("mongoose");
 
 // ENV Variables
 const port = process.env.PORT;
-const MONGODB_URI = process.env.MDB_COMPASS;
+// const MONGODB_URI = process.env.MDB_COMPASS;
+const MONGODB_URI = process.env.MDB_ATLAS;
 
 // Don't ask
 const app = express();
@@ -19,14 +20,14 @@ require("./routes/index.routes")(app);
 
 // Connect to DB and start server
 mongoose
-  .connect(MONGODB_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
-  .then(() => {
-    console.log("***Database Connected***");
-    app.listen(port, () => {
-      console.log(`<<<Server running on ${port}>>>`);
-    });
-  })
-  .catch((err) => console.log("Connection Error: ", err.message));
+    .connect(MONGODB_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    .then(() => {
+        console.log("***Database Connected***");
+        app.listen(port, () => {
+            console.log(`<<<Server running on ${port}>>>`);
+        });
+    })
+    .catch((err) => console.log("Connection Error: ", err.message));
