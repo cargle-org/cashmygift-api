9c955177f4bdb24193bb2bb9b76d5ea20250119b
Victory <victrudoh@gmail.com>
2023-03-28 18:53:47 +0100

    Added cmg fee update and fixed vouchers disappearing when another one was cashed

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,96 +5,96 @@ const moment = require("moment");
 
 // Models
 const User = require("../models/user.model");
+const voucherModel = require("../models/voucher.model");
 
 // Middlewares
 const asyncHandler = require("../middlewares/asyncHandler");
-const voucherModel = require("../models/voucher.model");
 
 module.exports = {
-  //   Test API connection
-  getPingController: (req, res) => {
-    try {
-      return res.status(200).send({
-        success: true,
-        message: "Pong",
-      });
-    } catch (err) {
-      return res.status(500).send({
-        success: false,
-        message: err.message,
-      });
-    }
-  },
+    //   Test API connection
+    getPingController: (req, res) => {
+        try {
+            return res.status(200).send({
+                success: true,
+                message: "Pong",
+            });
+        } catch (err) {
+            return res.status(500).send({
+                success: false,
+                message: err.message,
+            });
+        }
+    },
 
-  //   get user
-  getUserController: asyncHandler(async (req, res, next) => {
-    const { userId } = req.query;
+    //   get user
+    getUserController: asyncHandler(async(req, res, next) => {
+        const { userId } = req.query;
 
-    //   check if user exist
-    const user = await User.findOne({ _id: userId });
-    console.log(
-      "🚀 ~ file: user.controller.js:34 ~ getUserController:asyncHandler ~ user:",
-      user
-    );
-    if (!user) {
-      return res.status(400).send({
-        success: false,
-        message: "user not found.",
-      });
-    }
+        //   check if user exist
+        const user = await User.findOne({ _id: userId });
+        console.log(
+            "🚀 ~ file: user.controller.js:34 ~ getUserController:asyncHandler ~ user:",
+            user
+        );
+        if (!user) {
+            return res.status(400).send({
+                success: false,
+                message: "user not found.",
+            });
+        }
 
-    return res.status(200).send({
-      success: true,
-      data: {
-        user: user,
-      },
-      message: "fetched user successfully.",
-    });
-  }),
+        return res.status(200).send({
+            success: true,
+            data: {
+                user: user,
+            },
+            message: "fetched user successfully.",
+        });
+    }),
 
-  //   get all user vouchers
-  getAllUserVouchersController: asyncHandler(async (req, res, next) => {
-    const { userId } = req.query;
+    //   get all user vouchers
+    getAllUserVouchersController: asyncHandler(async(req, res, next) => {
+        const { userId } = req.query;
 
-    //   check if user exist
-    const vouchers = await voucherModel.find({ userId: userId });
+        //   check if user exist
+        const vouchers = await voucherModel.find({ userId: userId });
 
-    if (!vouchers) {
-      return res.status(400).send({
-        success: false,
-        message: "no vouchers found.",
-      });
-    }
+        if (!vouchers) {
+            return res.status(400).send({
+                success: false,
+                message: "no vouchers found.",
+            });
+        }
 
-    return res.status(200).send({
-      success: true,
-      data: {
-        vouchers: vouchers,
-      },
-      message: "fetched vouchers successfully.",
-    });
-  }),
+        return res.status(200).send({
+            success: true,
+            data: {
+                vouchers: vouchers,
+            },
+            message: "fetched vouchers successfully.",
+        });
+    }),
 
-  //   get one  vouchers
-  getOneVouchersController: asyncHandler(async (req, res, next) => {
-    const { voucherId } = req.query;
+    //   get one  vouchers
+    getOneVouchersController: asyncHandler(async(req, res, next) => {
+        const { voucherId } = req.query;
 
-    //   check if user exist
-    const voucher = await voucherModel.findOne({ _id: voucherId });
+        //   check if user exist
+        const voucher = await voucherModel.findOne({ _id: voucherId });
 
-    if (!voucher) {
-      return res.status(400).send({
-        success: false,
-        message: "voucher not found.",
-      });
-    }
+        if (!voucher) {
+            return res.status(400).send({
+                success: false,
+                message: "voucher not found.",
+            });
+        }
 
-    return res.status(200).send({
-      success: true,
-      data: {
-        voucher: voucher,
-      },
-      message: "fetched voucher successfully.",
-    });
-  }),
+        return res.status(200).send({
+            success: true,
+            data: {
+                voucher: voucher,
+            },
+            message: "fetched voucher successfully.",
+        });
+    }),
 };
--- services/flutterwave.services.js
+++ services/flutterwave.services.js
@@ -14,72 +14,76 @@ const FLW_secKey = process.env.FLUTTERWAVE_SECRET_KEY;
 const flw = new Flutterwave(FLW_pubKey, FLW_secKey);
 
 const options = {
-  timeout: 1000 * 60,
-  headers: {
-    "content-type": "application/json",
-    Authorization: `Bearer ${FLW_secKey}`,
-  },
+    timeout: 1000 * 60,
+    headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${FLW_secKey}`,
+    },
 };
 
-const initiateTransaction = async (payload) => {
-  try {
-    const response = await axios.post(`${baseURL}/payments`, payload, options);
-    // console.log(`Bearer ${FLW_secKey}`);
-    return response.data.data.link;
-  } catch (err) {
-    console.log(err);
-  }
+const initiateTransaction = async(payload) => {
+    try {
+        const response = await axios.post(`${baseURL}/payments`, payload, options);
+        // console.log(`Bearer ${FLW_secKey}`);
+        console.log(
+            "🚀 ~ file: flutterwave.services.js:28 ~ initiateTransaction ~ response:",
+            response
+        );
+        return response.data.data.link;
+    } catch (err) {
+        console.log(err);
+    }
 };
 
-const verifyTransaction = async (id) => {
-  try {
-    const response = await axios.get(
-      `${baseURL}/transactions/${id}/verify`,
-      options
-    );
-    console.log("verify: ", response.data);
-    return response.data.data;
-  } catch (err) {
-    console.log(err);
-  }
+const verifyTransaction = async(id) => {
+    try {
+        const response = await axios.get(
+            `${baseURL}/transactions/${id}/verify`,
+            options
+        );
+        console.log("verify: ", response.data);
+        return response.data.data;
+    } catch (err) {
+        console.log(err);
+    }
 };
 
-const transferMoney = async (payload) => {
-  try {
-    console.log("Processing Transfer...");
-    const response = await axios.post(`${baseURL}/transfers`, payload, options);
-    console.log(
-      "🚀 ~ file: flutterwave.services.js:52 ~ transferMoney ~ response:",
-      response.data.data
-    );
-    return response.data.data;
-  } catch (err) {
-    console.log("Transfer error: ", err.response);
-    // return res.status(400).send({
-    //   success: false,
-    //   message: "Error, couldn't process transfer",
-    //   errMessage: err.message,
-    // });
-  }
+const transferMoney = async(payload) => {
+    try {
+        console.log("Processing Transfer...");
+        const response = await axios.post(`${baseURL}/transfers`, payload, options);
+        console.log(
+            "🚀 ~ file: flutterwave.services.js:52 ~ transferMoney ~ response:",
+            response.data.data
+        );
+        return response.data.data;
+    } catch (err) {
+        console.log("Transfer error: ", err.response);
+        // return res.status(400).send({
+        //   success: false,
+        //   message: "Error, couldn't process transfer",
+        //   errMessage: err.message,
+        // });
+    }
 
-  // try {
-  //     const response = await flw.Transfer.initiate(payload);
-  //     console.log(
-  //         "🚀 ~ file: flutterwave.services.js:50 ~ transferMoney ~ response:",
-  //         response
-  //     );
-  //     return response.data;
-  // } catch (err) {
-  //     console.log(err);
-  //     return res.status(400).send({
-  //         success: false,
-  //         message: "Couldn't process transfer",
-  //     });
-  // }
+    // try {
+    //     const response = await flw.Transfer.initiate(payload);
+    //     console.log(
+    //         "🚀 ~ file: flutterwave.services.js:50 ~ transferMoney ~ response:",
+    //         response
+    //     );
+    //     return response.data;
+    // } catch (err) {
+    //     console.log(err);
+    //     return res.status(400).send({
+    //         success: false,
+    //         message: "Couldn't process transfer",
+    //     });
+    // }
 };
 
 module.exports = {
-  initiateTransaction,
-  verifyTransaction,
-  transferMoney,
+    initiateTransaction,
+    verifyTransaction,
+    transferMoney,
 };
