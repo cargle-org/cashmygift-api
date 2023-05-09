90b827d21b81de711b2e229029bab5d38eb5a68a
Edikan <victrudoh@gmail.com>
2023-05-09 10:13:40 +0100

    the monnify isn't working on live, redeploying to figure out why, also switched the mailer 'from' option to 'usepays.co' from 'cmg.co'

--- services/mailer.services.js
+++ services/mailer.services.js
@@ -2,23 +2,23 @@ const nodemailer = require("nodemailer");
 const dotenv = require("dotenv").config();
 
 let transporter = nodemailer.createTransport({
-    name: "mail.usepays.co",
-    host: "mail.usepays.co",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.MAIL_USER, //hi@usepays.co
-      pass: process.env.MAIL_PASSWORD, //(Gloed_c0)
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
+  name: "mail.usepays.co",
+  host: "mail.usepays.co",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.MAIL_USER, //hi@usepays.co
+    pass: process.env.MAIL_PASSWORD, //(Gloed_c0)
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 const sendMail = (mailOptions) => {
   transporter.sendMail(
     {
-      from: `"CMG.co" ${process.env.MAIL_USER}`,
+      from: `"usepays.co" ${process.env.MAIL_USER}`,
       to: mailOptions.to,
       subject: mailOptions.subject || "No Subject",
       html: mailOptions.html,
