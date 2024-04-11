5365fa99f06c10755e4cdf5cb922d17ce32fe5df
Edikan <victrudoh@gmail.com>
2024-04-11 09:57:54 +0100

    changed return link for verify email

--- templates/emailVerifyMail.templates.js
+++ templates/emailVerifyMail.templates.js
@@ -356,7 +356,7 @@ ul.social li{
 							
 							<h4>You recently began registration on CMG.co, please use the link below to complete your registration.
 							<br/>
-							 <h5><a href='https://cmg-three.vercel.app/accountverified?id=${id}&emailToken=${token}'>Click here to complete registration</a></h5>
+							 <h5><a href='https://usepays.co/accountverified?id=${id}&emailToken=${token}'>Click here to complete registration</a></h5>
 							<br><br>						
             			</div>
             		</td>
