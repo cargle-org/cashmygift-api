c8ee92efa1c85d37d2252d00eab8c73d24d904e0
Edikan <victrudoh@gmail.com>
2024-09-03 18:29:44 +0100

    UPDATE: Working on withdrawal feature on frontend so just in case

--- controllers/utils.controller.js
+++ controllers/utils.controller.js
@@ -168,11 +168,8 @@ const postFundWalletController = asyncHandler(async (req, res, next) => {
 
 // Verify "Fund wallet transaction"
 const getVerifyController = asyncHandler(async (req, res, next) => {
-  console.log("🚀 ~ getVerifyController ~ req.query:", req.query);
   const id = req.query.transaction_id ?? null;
-  console.log("🚀 ~ getVerifyController ~ id:", id);
   const tx_ref = req.query.tx_ref ?? null;
-  console.log("🚀 ~ getVerifyController ~ tx_ref:", tx_ref);
 
   if (!id || !tx_ref)
     return next(new ErrorResponse("Invalid query parameters", 400));
@@ -412,6 +409,7 @@ const verifyWalletFundWebhook = asyncHandler(async (req, res, next) => {
 const postWithdrawFromWalletController = asyncHandler(
   async (req, res, next) => {
     const { amount, bankCode, accountNumber } = req.body;
+    console.log("🚀 ~ req.body:", req.body);
 
     const transREf = await tx_ref.get_Tx_Ref();
 
