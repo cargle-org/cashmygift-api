b7048e042c57bcf2aefa726d3f980257d14e9892
Udoh Victory <victrudoh@gmail.com>
2024-10-05 15:01:11 +0100

    UPDATE: Switching mailer service to use app password

--- services/mailer.services.js
+++ services/mailer.services.js
@@ -2,26 +2,20 @@ const nodemailer = require("nodemailer");
 const dotenv = require("dotenv").config();
 
 let transporter = nodemailer.createTransport({
-  name: "mail.usepays.co",
-  host: "mail.usepays.co",
-  port: 465,
-  secure: true,
+  service: "gmail", // Use Gmail as the service
   auth: {
-    user: process.env.MAIL_USER, // hi@usepays.co
-    pass: process.env.MAIL_PASSWORD, // (Gloed_c0)
-  },
-  tls: {
-    rejectUnauthorized: false,
+    user: process.env.MAIL_USER, // chike.sn@gmail.com
+    pass: process.env.MAIL_PASSWORD, // Your Gmail app password
   },
 });
 
 const sendMail = (mailOptions) => {
   transporter.sendMail(
     {
-      from: `"usepays.co" ${process.env.MAIL_USER}`,
-      to: mailOptions.to,
-      subject: mailOptions.subject || "No Subject",
-      html: mailOptions.html,
+      from: `"UsePays.co" <${process.env.MAIL_USER}>`, // Sender's email
+      to: mailOptions.to, // Recipient's email
+      subject: mailOptions.subject || "No Subject", // Email subject
+      html: mailOptions.html, // HTML content of the email
     },
     function (error, info) {
       if (error) {
@@ -34,3 +28,40 @@ const sendMail = (mailOptions) => {
 };
 
 module.exports = sendMail;
+
+// const nodemailer = require("nodemailer");
+// const dotenv = require("dotenv").config();
+
+// let transporter = nodemailer.createTransport({
+//   name: "mail.usepays.co",
+//   host: "mail.usepays.co",
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: process.env.MAIL_USER, // hi@usepays.co
+//     pass: process.env.MAIL_PASSWORD, // (Gloed_c0)
+//   },
+//   tls: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+// const sendMail = (mailOptions) => {
+//   transporter.sendMail(
+//     {
+//       from: `"usepays.co" ${process.env.MAIL_USER}`,
+//       to: mailOptions.to,
+//       subject: mailOptions.subject || "No Subject",
+//       html: mailOptions.html,
+//     },
+//     function (error, info) {
+//       if (error) {
+//         console.log(error);
+//       } else {
+//         console.log("Email sent: " + info.response);
+//       }
+//     }
+//   );
+// };
+
+// module.exports = sendMail;
