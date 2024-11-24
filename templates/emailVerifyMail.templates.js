2473345a872d18b136ace5d6557ef4227a9234e6
Udoh Victory <victrudoh@gmail.com>
2024-11-24 17:49:35 +0100

    UPDATE: switched email reset password url

--- templates/emailVerifyMail.templates.js
+++ templates/emailVerifyMail.templates.js
@@ -356,7 +356,6 @@ ul.social li{
 							
 							<h4>You recently began registration on CMG.co, please use the link below to complete your registration.
 							<br/>
-							 <!-- <h5><a href='https://www.usepays.co/auth/verify?id=${id}&emailToken=${token}'>Click here to complete registration</a></h5> -->
 							 <h5><a href='https://www.usepays.co/auth/verify?id=${id}&emailToken=${token}'>Click here to complete registration</a></h5>
 							<br><br>						
             			</div>
