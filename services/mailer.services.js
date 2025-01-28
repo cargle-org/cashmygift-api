8b3939c344467d0e87f3eaa2a3e469606986373b
Johnson Olaoluwa <johnsonolaolu@gmail.com>
2025-01-28 12:34:23 +0100

    fix: revert back to gmail

--- services/mailer.services.js
+++ services/mailer.services.js
@@ -2,23 +2,23 @@ const nodemailer = require("nodemailer");
 const dotenv = require("dotenv").config();
 
 let transporter = nodemailer.createTransport({
-  // service: "gmail", // Use Gmail as the service
-  // auth: {
-  //   user: process.env.MAIL_USER, // chike.sn@gmail.com
-  //   pass: process.env.MAIL_PASSWORD, // Your Gmail app password
-  // },
-  name: "mail.usepays.co",
-  host: "mail.usepays.co", // Replace with your Bluehost mail host
-  port: 465, // Use 587 for TLS
-  secure: true, // Set to false if using port 587
-  requireTLS: true,
+  service: "gmail", // Use Gmail as the service
   auth: {
-    user: process.env.MAIL_USER, // Your domain email
-    pass: process.env.MAIL_PASSWORD, // Your email password
+    user: process.env.MAIL_USER, // chike.sn@gmail.com
+    pass: process.env.MAIL_PASSWORD, // Your Gmail app password
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
+  // name: "mail.usepays.co",
+  // host: "mail.usepays.co", // Replace with your Bluehost mail host
+  // port: 465, // Use 587 for TLS
+  // secure: true, // Set to false if using port 587
+  // requireTLS: true,
+  // auth: {
+  //   user: process.env.MAIL_USER, // Your domain email
+  //   pass: process.env.MAIL_PASSWORD, // Your email password
+  // },
+  // tls: {
+  //   rejectUnauthorized: false,
+  // },
 });
 
 const sendMail = (mailOptions) => {
