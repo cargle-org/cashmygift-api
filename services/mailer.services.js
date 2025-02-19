edd462e8d731e31138ff4ab061bbec6cc1070f4e
CodedLibra <adeola.ogundijo@yahoo.com>
2025-02-19 16:58:33 +0100

    chore: reconfigured the nodemailer transport, added a paysLogo attachment and removed unused files

--- services/mailer.services.js
+++ services/mailer.services.js
@@ -1,33 +1,35 @@
-const nodemailer = require("nodemailer");
-const dotenv = require("dotenv").config();
+const path = require('path');
+const nodemailer = require('nodemailer');
 
-let transporter = nodemailer.createTransport({
-  // service: "gmail", // Use Gmail as the service
-  // auth: {
-  //   user: process.env.MAIL_USER, // chike.sn@gmail.com
-  //   pass: process.env.MAIL_PASSWORD, // Your Gmail app password
-  // },
-  name: "mail.usepays.co",
-  host: "mail.usepays.co", // Replace with your Bluehost mail host
-  port: 465, // Use 587 for TLS
-  secure: true, // Set to false if using port 587
-  requireTLS: true,
+const transporter = nodemailer.createTransport({
+  host: 'smtp.improvmx.com',
+  port: 587, // or 465 (for SSL)
+  secure: false, // true for 465, false for other ports
   auth: {
-    user: process.env.MAIL_USER, // Your domain email
-    pass: process.env.MAIL_PASSWORD, // Your email password
-  },
-  tls: {
-    rejectUnauthorized: false,
+    user: process.env.MAIL_USER, // Replace with your Improvmx username
+    pass: process.env.MAIL_PASSWORD, // Replace with your Improvmx password
   },
 });
+
+
+const paysLogo = path.join(__dirname, "..", 'public', 'images', 'pays_logo.png');
 
 const sendMail = (mailOptions) => {
   transporter.sendMail(
     {
       from: `"UsePays.co" <${process.env.MAIL_USER}>`, // Sender's email
       to: mailOptions.to, // Recipient's email
-      subject: mailOptions.subject || "No Subject", // Email subject
+      subject: mailOptions.subject, // Email subject
       html: mailOptions.html, // HTML content of the email
+      head: mailOptions.html, // HTML content of the email
+      attachments: [
+        {
+          // Pays Logo
+          filename: 'pays_logo.png',
+          path: paysLogo,
+          cid: 'pays-logo-cid',
+        },
+      ],
     },
     function (error, info) {
       if (error) {
@@ -41,39 +43,3 @@ const sendMail = (mailOptions) => {
 
 module.exports = sendMail;
 
-// const nodemailer = require("nodemailer");
-// const dotenv = require("dotenv").config();
-
-// let transporter = nodemailer.createTransport({
-//   name: "mail.usepays.co",
-//   host: "mail.usepays.co",
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     user: process.env.MAIL_USER, // hi@usepays.co
-//     pass: process.env.MAIL_PASSWORD, // (Gloed_c0)
-//   },
-//   tls: {
-//     rejectUnauthorized: false,
-//   },
-// });
-
-// const sendMail = (mailOptions) => {
-//   transporter.sendMail(
-//     {
-//       from: `"usepays.co" ${process.env.MAIL_USER}`,
-//       to: mailOptions.to,
-//       subject: mailOptions.subject || "No Subject",
-//       html: mailOptions.html,
-//     },
-//     function (error, info) {
-//       if (error) {
-//         console.log(error);
-//       } else {
-//         console.log("Email sent: " + info.response);
-//       }
-//     }
-//   );
-// };
-
-// module.exports = sendMail;
